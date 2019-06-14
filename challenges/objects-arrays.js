383ddfdb1a9dc8d48bf798c3c9f181608373eb33
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-14 09:46:32 -0700

    arrays request 3 complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,7 +90,7 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(str => str.university.includes('Uni'));
 console.log(uni);
 
 
