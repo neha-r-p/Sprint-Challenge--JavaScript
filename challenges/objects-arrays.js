a7a587598e65e89812a4f7b9f0239f885f7e457e
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-14 09:13:57 -0700

    tyrannosaurus and stegosaurus object created

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,7 +8,21 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = {
+  diet:'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+
+const stegosaurus = {
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
