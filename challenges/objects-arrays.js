9242744df25b6c3aeade4e6ce818b9111c398ba0
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-14 09:24:05 -0700

    finished dino object section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -49,7 +49,11 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+tyrannosaurus.roar = function(){
+  return "RAWERSRARARWERSARARARRRR!";
+}
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
